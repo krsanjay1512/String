570fb5bffded7b2979e667dee68cad955e6246e7
Sanjay Kumar <84794705+krsanjay1512@users.noreply.github.com>
2021-11-06 23:07:13 +0530

    Create Pangram.cs

---
+++ Pangram.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the string :");
+            string input = Console.ReadLine();
+            string alpha = "asdfghjklzxcvbnmqwertyuiop";
+            string result = "panagram";
+            for (int i=0;i<alpha.Length;i++)
+            {
+                if (!input.ToLower().Contains(alpha[i]))
+                {
+                    result="Not Pangram";
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
