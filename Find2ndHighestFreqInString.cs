827b08e70d9c989d41add8913b0ffb552b295d71
Sanjay Kumar <84794705+krsanjay1512@users.noreply.github.com>
2022-07-30 23:47:43 +0530

    Create Find2ndHighestFreqInString.cs

---
+++ Find2ndHighestFreqInString.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find2ndHighestFreqInString
+{
+    class Program
+    {
+               
+        static void Main(string[] args)
+        {
+            string input = "hgffsdghfdshgg";
+            int first = 0, second = 0;
+            int[] arr = new int[256];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                arr[input[i]]++;
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                if (arr[i] > arr[first])
+                {
+                    second = first;
+                    first = i;
+                }
+                else if (arr[i] > arr[second] && arr[i] != arr[first])
+                {
+                    second = i;
+
+                }
+
+            }
+            Console.WriteLine((char)second);
+            Console.ReadLine();
+        }
+    }
+}
